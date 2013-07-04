543c46841f3b1dedcf1d06bd0f8fcd73b011e390
GameFrame <kahr98@hotmail.com>
2013-07-04 17:40:20 +0200

    Gameobject class

---
+++ Gameobject.java
@@ -0,0 +1,18 @@
+package org.gameframe.greydecay.engine;
+
+public abstract class GameObject {
+
+  private float x;
+	private float y;
+	private float sx;
+	private float sy;
+	private Animation anim;
+	
+	public void update(){
+		
+	}
+	
+	public void render(){
+		
+	}
+}
